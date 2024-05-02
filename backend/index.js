75fff9eeaa2d510458ead73c49304abbffa4bc96
Prakash <prakashdevnani001@gmail.com>
2024-05-02 10:40:29 +0530

    Change PORT

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const client = require("./db");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 const contactModel = require("./schema");
 app.use(bodyParser.json());
@@ -16,18 +16,18 @@ const getCurrentDate = () => {
   return currentDate;
 };
 
-const getMongoCollectionOfContacts=async ()=>{
+const getMongoCollectionOfContacts = async () => {
   const mongoClient = await client();
-  currentClientInstance=mongoClient;
+  currentClientInstance = mongoClient;
   const db = mongoClient.db("contact-information");
   const collection = db.collection("contacts");
   return collection;
-}
+};
 
 app.post("/add-contact", async (req, res) => {
   const { name, contactNumber, country, tag, dateOfBilling } = req.body;
-  const collection=await getMongoCollectionOfContacts();
-  
+  const collection = await getMongoCollectionOfContacts();
+
   try {
     const newContact = await collection.insertOne({
       name,
@@ -45,13 +45,11 @@ app.post("/add-contact", async (req, res) => {
 });
 
 app.get("/all-contacts", async (req, res) => {
-  const collection=await getMongoCollectionOfContacts();
-  const allRecords=await collection.find({}).toArray();
-  res.status(200).json({ allRecords});
+  const collection = await getMongoCollectionOfContacts();
+  const allRecords = await collection.find({}).toArray();
+  res.status(200).json({ allRecords });
 });
 
-
-
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
